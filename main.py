ae1d5d7b3ebe3872abd095f9d948784572fb661e
Alex <alex.altamirandav@proton.me>
2023-12-07 00:23:56 -0500

    Updating UserAgent

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+#Iphone's User-Agent is to evade CloudFlare
 headers = {
-  'User-Agent': 'Elliot Phone',
+  'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1',
   'Referer': 'https://noidea.ip',
   'Origin': 'https://noidea.ip'
 }
